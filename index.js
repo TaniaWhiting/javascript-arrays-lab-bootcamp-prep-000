b703f837397ee47e9322974cca04c1eef7931c44
Tania Whiting <tanwhiting@aol.com>
2018-08-15 22:31:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+var kittens = ["Milo","Otis","Garfield"]
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+
+
+
+
+
+
 // Add your functions and code here
